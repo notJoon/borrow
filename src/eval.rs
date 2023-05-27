9315bdb25653c5413a9daa56bea7f03128605390
not_joon <lbj199874@gmail.com>
2023-05-27 21:57:37 +0900

    add comments

--- src/eval.rs
+++ src/eval.rs
@@ -1,37 +1,53 @@
 use std::collections::HashMap;
 
-use crate::ast::{Statement, Expression, BinaryOp};
+use crate::ast::{BinaryOp, Expression, Statement};
 
+/// The `Evaluator` struct will store the state of the program during evaluation.
+///
+/// This includes the global env, which keeps track of all the global variables,
+/// and the local env stack, which keeps track of all the local variables.
 pub struct Evaluator {
+    /// The global env maps variable names to their values
     global_env: HashMap<String, i32>,
+    /// A stack of local envs for nested scopes
     local_env: Vec<HashMap<String, i32>>,
 }
 
 impl Evaluator {
+    /// The constructor initializes an evaluator
+    /// with an empty global environment and one empty local environment.
     pub fn new() -> Self {
-        Evaluator { 
-            global_env: HashMap::new(), 
-            local_env: vec![HashMap::new()] 
+        Evaluator {
+            global_env: HashMap::new(),
+            local_env: vec![HashMap::new()],
         }
     }
 
+    /// The evaluate method takes a list of statements (the program),
+    /// and evaluates each statement in turn.
+    ///
+    /// It returns the value of the last statement if exists.
     pub fn evaluate(&mut self, stmts: &[Statement]) -> Result<i32, String> {
         for stmt in stmts {
             self.evaluate_stmt(stmt)?;
         }
 
-        Ok(0)
+        Ok(0) // return `0` after successfully evaluating all statements
     }
 
+    /// `evaluate_stmt` evaluates a single statement.
+    ///
+    /// It dispatches to the appropriate handler method based on the type of statement.
     fn evaluate_stmt(&mut self, stmt: &Statement) -> Result<(), String> {
         match stmt {
+            // For variable declarations, evaluate the value (if any), then insert it into the current scope.
             Statement::VariableDecl { name, value, .. } => {
                 let value = match value {
                     Some(value) => self.evaluate_expr(value)?,
-                    None => 0,
+                    None => 0, // default value is `0`
                 };
 
-                self.global_env.insert(name.clone(), value);
+                self.insert_global(name.clone(), value);
 
                 Ok(())
             }
@@ -39,17 +55,19 @@ impl Evaluator {
                 // we are not evaluate function definition at runtime
                 Ok(())
             }
-            Statement::FunctionCall { .. } =>  {
+            Statement::FunctionCall { .. } => {
                 unimplemented!("Evaluator::evaluate_stmt: FunctionCall")
             }
+            // For scope statements, create a new local environment, evaluate all statements
+            // within the scope, then discard the local environment.
             Statement::Scope(stmts) => {
-                self.local_env.push(HashMap::new());
+                self.local_env.push(HashMap::new()); // Create a new local environment.
 
                 for stmt in stmts {
                     self.evaluate_stmt(stmt)?;
                 }
 
-                self.local_env.pop();
+                self.local_env.pop(); // Discard the local environment after exiting the scope.
 
                 Ok(())
             }
@@ -59,8 +77,11 @@ impl Evaluator {
         }
     }
 
+    // `evaluate_expr` evaluates an expression and returns its value.
+    // It handles identifiers, numbers, and binary operations.
     fn evaluate_expr(&self, expr: &Expression) -> Result<i32, String> {
         match expr {
+            // For identifiers, look them up in the current scope, then in the global scope.
             Expression::Ident(name) => {
                 for env in self.local_env.iter().rev() {
                     if let Some(value) = env.get(name) {
@@ -68,14 +89,16 @@ impl Evaluator {
                     }
                 }
 
+                // If the identifier is not found in any local scope, look it up in the global scope.
                 self.global_env.get(name).copied().ok_or(format!(
                     "undefined variable `{}` not found (evaluating expression)",
                     name
                 ))
             }
-            Expression::Number(value) => {
-                Ok(*value)
-            },
+            // Numbers are returned as is.
+            Expression::Number(value) => Ok(*value),
+            // Binary operations are evaluated by recursively evaluating the left and right sides,
+            // then applying the operator to the results.
             Expression::BinaryOp { lhs, op, rhs } => {
                 let lhs = self.evaluate_expr(lhs)?;
                 let rhs = self.evaluate_expr(rhs)?;
@@ -90,6 +113,11 @@ impl Evaluator {
             _ => Err(format!("invalid or unsupported expression: {:?}", expr)),
         }
     }
+
+    /// insert_global inserts a new variable into the global environment.
+    fn insert_global(&mut self, name: String, value: i32) -> Option<i32> {
+        self.global_env.insert(name, value)
+    }
 }
 
 #[cfg(test)]
@@ -103,15 +131,15 @@ mod eval_test {
         // let x = 5;
         // let y = 10;
         let stmts = vec![
-            Statement::VariableDecl { 
-                name: "x".to_string(), 
-                value: Some(Expression::Number(5)), 
-                is_borrowed: false, 
+            Statement::VariableDecl {
+                name: "x".to_string(),
+                value: Some(Expression::Number(5)),
+                is_borrowed: false,
             },
-            Statement::VariableDecl { 
-                name: "y".to_string(), 
-                value: Some(Expression::Number(10)), 
-                is_borrowed: false, 
+            Statement::VariableDecl {
+                name: "y".to_string(),
+                value: Some(Expression::Number(10)),
+                is_borrowed: false,
             },
         ];
 
@@ -127,20 +155,20 @@ mod eval_test {
         let mut eval = Evaluator::new();
 
         let stmts = vec![
-            Statement::VariableDecl { 
-                name: "x".to_string(), 
+            Statement::VariableDecl {
+                name: "x".to_string(),
                 value: Some(Expression::Number(5)),
-                is_borrowed: false, 
-            },
-            Statement::VariableDecl { 
-                name: "y".to_string(),
-                value: Some(Expression::BinaryOp { 
-                    lhs: Box::new(Expression::Ident("x".to_string())),
-                    op: BinaryOp::Plus, 
-                    rhs: Box::new(Expression::Number(10)), 
-                }), 
                 is_borrowed: false,
-            }
+            },
+            Statement::VariableDecl {
+                name: "y".to_string(),
+                value: Some(Expression::BinaryOp {
+                    lhs: Box::new(Expression::Ident("x".to_string())),
+                    op: BinaryOp::Plus,
+                    rhs: Box::new(Expression::Number(10)),
+                }),
+                is_borrowed: false,
+            },
         ];
 
         let result = eval.evaluate(&stmts);
