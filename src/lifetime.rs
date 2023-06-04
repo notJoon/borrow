bf76151dd0e183af20a34bb7e675aaeefee28879
not_joon <lbj199874@gmail.com>
2023-06-04 13:53:07 +0900

    add lifetime basic logics WIP

--- src/lifetime.rs
+++ src/lifetime.rs
@@ -1,27 +1,38 @@
 use std::collections::BTreeMap;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 use crate::borrow_checker::BorrowState;
+use crate::errors::LifetimeError;
+
+/// Automatically generate a unique scope `id`.
+static SCOPE_ID: AtomicUsize = AtomicUsize::new(0);
+
+/// Generate a unique scope `id`.
+fn next_scope_id() -> usize {
+    SCOPE_ID.fetch_add(1, Ordering::SeqCst)
+}
 
 /// A variable in the scope.
-/// 
+///
 /// It makes the `Scope` more flexible rather than directly using `BorrowState`.
 pub struct Variable {
     state: BorrowState,
+    scope_id: usize,
 }
 
 /// `Scope` is a collection of variables.
-/// 
+///
 /// It is used to track the variables in the current scope.
 pub struct Scope<'a> {
+    id: usize,
     variables: BTreeMap<&'a str, Variable>,
+    parent: Option<&'a Scope<'a>>,
 }
 
 impl Variable {
     /// Creates a new `Variable` instance.
-    pub fn new(is_borrowed: bool) -> Result<Self, String> {
-        Ok(Self {
-            state: BorrowState::Uninitialized,
-        })
+    pub fn new(state: BorrowState, scope_id: usize) -> Self {
+        Self { state, scope_id }
     }
 
     pub fn get_state(&self) -> &BorrowState {
@@ -31,13 +42,23 @@ impl Variable {
     pub fn set_state(&mut self, state: BorrowState) {
         self.state = state;
     }
+
+    pub fn get_scope_id(&self) -> usize {
+        self.scope_id
+    }
+
+    pub fn set_scope_id(&mut self, scope_id: usize) {
+        self.scope_id = scope_id;
+    }
 }
 
 impl<'a> Scope<'a> {
     /// Creates a new `scope` instance.
     pub fn new() -> Self {
         Self {
+            id: next_scope_id(),
             variables: BTreeMap::new(),
+            parent: None,
         }
     }
 
@@ -47,16 +68,17 @@ impl<'a> Scope<'a> {
     }
 
     /// Insert a variable and borrow state into the scope.
-    pub fn insert(&mut self, var: &'a str, state: BorrowState) {
-        let variable = Variable { state };
+    pub fn insert(&'a mut self, var: &'a str, state: BorrowState) -> Result<(), LifetimeError> {
+        if self.contains_val(var) {
+            return Err(LifetimeError::VariableAlreadyExists(var.to_string()));
+        }
 
-        self.variables.insert(var, variable);
+        self.variables.insert(var, Variable::new(state, self.id));
+        Ok(())
     }
 
     pub fn get_state(&self, var: &'a str) -> Option<&BorrowState> {
-        self.variables
-            .get(var)
-            .map(|v| v.get_state())
+        self.variables.get(var).map(|v| v.get_state())
     }
 
     pub fn set_state(&mut self, var: &'a str, state: BorrowState) {
@@ -65,16 +87,28 @@ impl<'a> Scope<'a> {
         }
     }
 
-    /// Compute the borrowed variables in the scope
-    pub fn borrowed_vars(&self) -> Vec<&'a str> {
+    pub fn check_lifetime(& self, var: &'a str, borrow_id: usize) -> Result<(), LifetimeError> {
+        let variable = self.get_variable(var)?;
+
+        if borrow_id < variable.scope_id {
+            return Err(LifetimeError::LifetimeTooShort(var.to_string()));
+        }
+
+        Ok(())
+    }
+
+    pub fn check_borrow_rules(&self, var: &'a str) -> Result<(), LifetimeError> {
+        let variable = self.get_variable(var)?;
+
+        match variable.state {
+            BorrowState::Borrowed => Err(LifetimeError::BorrowedMutable(var.to_string())),
+            _ => Ok(()),
+        }
+    }
+
+    fn get_variable(&self, var: &'a str) -> Result<&Variable, LifetimeError> {
         self.variables
-            .iter()
-            .filter_map(|(&var, variable)| {
-                match variable.state {
-                    BorrowState::Borrowed(_) | BorrowState::ImmutBorrowed(_) => Some(var),
-                    _ => None,
-                }
-            })
-            .collect()
+            .get(var)
+            .ok_or(LifetimeError::VariableNotFound(var.to_string()))
     }
 }
